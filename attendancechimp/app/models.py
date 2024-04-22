d6a6875541f8f3e5466def4da9c1aadf227d1cc6
NatiBerhe <nberhe@uchicago.edu>
2024-04-21 23:38:25 -0500

    This is the working solution I came up with, please let me know your feedback

--- attendancechimp/app/models.py
+++ attendancechimp/app/models.py
@@ -1,48 +1,146 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.crypto import get_random_string
 
-# Create your models here.
-class Student(models.Model):
-    studname = models.CharField(max_length=70, null = False)
-    studentid = models.IntegerField(primary_key = True)
-    def __str__(self):
-        return self.name
-class Instructor(models.Model):
-    instname = models.CharField(max_length=256,null = False)
-    instid = models.IntegerField(primary_key = True)
-    def __str__(self):
-        return self.name
+class UniversityPerson(models.Model):
+    """This model describes a university person, either a student
+    or an instructor. It contains all of the necessary data that
+    might identify such a person.
+
+    Note: We link this entity to the Django user model so we only
+    need to store information unique to a "UniversityPerson" above
+    and beyond a simple user.
+    """
+
+    # an auto incrementing id
+    auto_increment_id = models.AutoField(primary_key=True)
+
+    # is an instructor or student?
+    is_instructor = models.BooleanField(null=False)
+
+    # relate to django auth user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# for each model, I like to wrap it in a simple helper function
+# for example for the user model, when creating a user we create
+# two users one university person and one django user.
+def create_ac_user(name, email, password, is_instructor):
+    """Creates an ac user and a corresponding django user"""
+
+    # first create django user
+    user = User.objects.create_user(name, email, password)
+    user.save()
+
+    # then create ac user
+    up = UniversityPerson(is_instructor=is_instructor, user=user)
+    up.save()
+
+    # return both users
+    return user, up
+
+
 class Course(models.Model):
-    name = models.CharField(max_length=256, primary_key=True)
-    instructor = models.OneToOneField(Instructor, on_delete=models.CASCADE)
-    students = models.ManyToManyField(Student)
+    """A course represents a single course using attendancechimp. A course
+    stores a reference to the instructor as well as the times/days of the
+    week that it meets."""
 
-    def __str__(self):
-        return self.name
+    # an internal unique id
+    auto_increment_id = models.AutoField(primary_key=True, default=1)
 
-class Lecture(models.Model):
-    title = models.CharField(max_length=200)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    date = models.DateField()
+    # a course name
+    name = models.CharField(max_length=128)
 
-    def __str__(self):
-        return self.title
-    
-class Attendance(models.Model):
-    lecture_id = models.AutoField(primary_key=True)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    lecture_date = models.DateField()
+    # foreign key to instructor
+    instructor = models.ForeignKey(UniversityPerson, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.title
+    # class time start and end
+    class_start = models.TimeField()
+    class_end = models.TimeField()
+
+    # class days
+    m_class = models.BooleanField(default=False)
+    tu_class = models.BooleanField(default=False)
+    w_class = models.BooleanField(default=False)
+    th_class = models.BooleanField(default=False)
+    f_class = models.BooleanField(default=False)
+
+
+# let's spec out the basic functionality of a course
+def create_course(name, instructor, days, start, end):
+    """Creates an ac user and a corresponding django user"""
+    course = Course(name=name, instructor=instructor, class_start=start, class_end=end)
+
+    # handles course meetings
+    for d in days:
+        if d == "M":
+            course.m_class = True
+
+        if d == "Tu":
+            course.tu_class = True
+
+        if d == "W":
+            course.w_class = True
+
+        if d == "Th":
+            course.th_class = True
+
+        if d == "F":
+            course.f_class = True
+
+    course.save()
+
+    return course
+
 
 class QRCode(models.Model):
-    qr_code_id = models.AutoField(primary_key=True)
-    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, null=True, blank=True)
-    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE, null=True, blank=True)
-    upload_datetime = models.DateTimeField(auto_now_add=True)
-    qr_code_image = models.ImageField(upload_to='qr_codes/')
-    is_valid = models.BooleanField(default=True)
-    
-    def __str__(self):
-        return self.title
+    """A qr code is tied to a particular lecture. Note this object just defines the
+    QR code does not contain any image data.
+    """
+
+    # an internal unique id
+    auto_increment_id = models.AutoField(primary_key=True, default=1)
+
+    # linked to a course
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    # code
+    code = models.CharField(max_length=32)
+
+    # this is a trick to automatically generate a random
+    # string on save
+    def save(self, *args, **kwargs):
+        self.code = get_random_string(length=32)
+        super(QRCode, self).save(*args, **kwargs)
+
+
+# this is the functionality to create a qr code
+def create_qr_code(course):
+    qrcode = QRCode(course=course)
+    qrcode.save()
+    return qrcode
+
+
+class QRCodeUpload(models.Model):
+    """This model represents a particular qrcode upload from a student."""
+
+    # an internal unique id
+    auto_increment_id = models.AutoField(primary_key=True, default=1)
+
+    # linked to a course
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    # linked to a student
+    student = models.ForeignKey(UniversityPerson, on_delete=models.CASCADE)
+
+    # data
+    image = models.ImageField(upload_to="data")
+
+    # time stamp on upload
+    uploaded = models.DateTimeField(auto_now_add=True)
+
+
+# this is the functionality to process an upload
+def process_upload(course, student, image):
+    upload = QRCodeUpload(course=course, student=student, image=image)
+    upload.save()
+    return upload
--- attendancechimp/app/views.py
+++ attendancechimp/app/views.py
@@ -2,14 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
+from django.contrib.auth.models import User
+from .models import UniversityPerson
+import time
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
+from datetime import datetime
+from django.contrib import messages
+
+
+
 
 @csrf_exempt
 def index(request):
-    return render(request, 'app/index.html', {})
-
+    if request.method == "GET":
+        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Get current time
+        return render(request, 'app/index.html', {'current_time': current_time})
+    
 @csrf_exempt
 def handle_form(request):
-
     cname = request.POST['cname']
     cnum =  request.POST['cnum']
 
@@ -19,3 +30,40 @@ def handle_form(request):
     new_course.save()
 
     return render(request, 'app/index.html', {})
+
+def new(request):
+    if request.method == 'GET' and not request.user.is_authenticated:
+        return render(request, 'app/new.html')
+    return PermissionDenied
+
+def create_ac_user(name, email, password, is_instructor):
+    user = User.objects.create_user(username=name, email=email, password=password)
+    user.save()
+
+    np = UniversityPerson.objects.create(user=user, is_instructor=is_instructor)
+    np.save()
+
+    return user, np
+
+def create_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        is_instructor = (request.POST.get("choice") == "instructor")
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Email already in use')
+            return render(request, 'app/new.html',{'error': 'Email already in use'})
+
+        user, np = create_ac_user(username, email, password, is_instructor)
+        user.save()
+        login(request, user)
+        
+        messages.success(request, 'User created successfully!')
+        return redirect('index')
+                
+    else:
+        return render(request, 'app/new.html', {'error':'invalid request'})
